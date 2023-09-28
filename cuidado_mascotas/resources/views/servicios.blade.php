b6960fd0d806d85e70e58ec9af23bb1d036b757d
julieta bariandaran <julietabdr@icloud.com>
2023-09-28 11:16:42 -0300

    servicios

---
+++ cuidado_mascotas/resources/views/servicios.blade.php
@@ -0,0 +1,76 @@
+<div id="servicios">
+<div class="container px-4 py-5" id="featured-3">
+    <h2 class="pb-2 border-bottom">Nuestros Servicios</h2>
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Paseadores</h3>
+        <p>Ofrecemos un servicio de paseo que combina diversión y cuidado. Nuestros paseadores de perros apasionados y confiables están listos para dar a tu mascota la atención y el ejercicio que se merece. </p>
+        <a href="#" class="icon-link">
+        Lo quiero
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a>
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Hoteles</h3>
+        <p>Nuestro hotel es el lugar perfecto para que tu mascota se sienta como en casa mientras estás fuera. Ofrecemos un ambiente seguro y acogedor, con amplias instalaciones y un equipo amable y dedicado. Tu mascota disfrutará de comodidades de primera clase y cuidados personalizados.</p>
+        <a href="#" class="icon-link">
+        Lo quiero
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a>
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Baños</h3>
+        <p>Nuestro servicio de baños es un mimo refrescante para tu mascota. Ofrecemos tratamientos de aseo de alta calidad que incluyen baños relajantes, cepillado, corte de uñas y más.¡Listo para recibir abrazos y caricias!</p>
+        <a href="#" class="icon-link">
+          Lo quiero
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a>
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Transporte</h3>
+        <p>Nuestro servicio de transporte está diseñado para brindar comodidad y seguridad a tus mascotas. Ofrecemos viajes confiables y cómodos para llevar a tu mascota al veterinario, la peluquería o cualquier otro destino necesario. Tu mascota viajará con cuidado y atención en cada trayecto. </p>
+        <a href="#" class="icon-link">
+        Lo quiero
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a>
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Guarderia</h3>
+        <p>Nuestra guardería para mascotas es el lugar ideal para que tu perro socialice y se divierta mientras tú estás fuera. Ofrecemos un entorno seguro y estimulante donde las mascotas pueden jugar, recibir atención constante y hacer nuevos amigos peludos, todo bajo la supervisión de nuestro equipo experimentado.</p>
+        <a href="#" class="icon-link">
+        Lo quiero
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a>
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
+        </div>
+        <h3 class="fs-2 text-body-emphasis">Entrenamiento</h3>
+        <p>Nuestro servicio de entrenamiento de mascotas se enfoca en cultivar comportamientos positivos y habilidades de obediencia en tu perro. Nuestros entrenadores altamente capacitados utilizan métodos efectivos y amigables para ayudar a tu mascota a convertirse en un compañero bien educado y equilibrado.</p>
+        <a href="#" class="icon-link">
+          Lo quiero
+          <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+        </a>
+      </div>
+    </div>
+  </div>
+
+
+ 
+      
--- cuidado_mascotas/resources/views/visitor/index.blade.php
+++ cuidado_mascotas/resources/views/visitor/index.blade.php
@@ -5,15 +5,34 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Mascotas</title>
+  <title>SniffNear</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 
 <body>
 
-<h1> cuidado mascotas </h1>
+  <!-- ======= Header ======= -->
+  <header id="header">
+    @include('header')
+  </header><!-- End Header -->
+ 
+  <main id="main">
+  <div id="servicios">
+  @include('servicios')
+  </div>
+</main>
+<footer id="footer" class="footer">
+  <!-- ======= Footer ======= -->
+  @include('footer')
+  <!-- End Footer -->
+</footer>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
+
+  <!-- <script src="{{ asset('/js/main.js')}}"></script> -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
